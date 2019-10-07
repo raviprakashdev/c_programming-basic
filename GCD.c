0069c5bcdf330598af52e66d73584340e7d4ce1f
jeremie1112 <jeremie.daniel@hotmail.com>
2019-10-07 20:02:34 +0400

    The GCD.c file code has been refactored

--- GCD.c
+++ GCD.c
@@ -11,13 +11,8 @@ int main()
 }
 int gcd(int n1 ,int n2)
 {
-	int rem;
-	rem=n1%n2;
-	if(rem==0)
-	{
-		return (n2);
-	}
-	else
-	return (gcd(n2,rem));
+	if (n2 != 0)
+       return gcd(n2, n1%n2);
+    else 
+       return n1;
 }
-
