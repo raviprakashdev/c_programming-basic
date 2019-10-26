1b3d5ae63f22c3c264010503a73345b59b40fd8c
34Tarik <57030321+34Tarik@users.noreply.github.com>
2019-10-26 12:53:56 +0300

    Add files via upload

---
+++ Find_the_max_number_of_three.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+
+{
+	int a,b,c;
+	
+	printf("Enter 3 numbers: ");
+	scanf("%d,%d,%d",&a,&b,&c);
+	
+	if(a>b && a>c)
+	{
+		printf("your first entryis MAX = %d");
+	}
+		if(b>a && b>c)
+		{
+			printf("your second entry is MAX = %d");
+		}
+			if(c>a && c>b)
+			{
+				printf("your third entry is MAX = %d");
+			}
+		
+	return 0;
+}
