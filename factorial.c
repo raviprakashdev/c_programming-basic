c811a42e1148022d6901c0385dd72c03ff19836d
arnavbansal2764 <70932975+arnavbansal2764@users.noreply.github.com>
2020-10-01 08:58:30 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,13 +1,19 @@
+//This program will tell the factorial of number input by the user
 #include<stdio.h>
-int main()
-{
-int i,f=1,n;
-printf("Enter the no.\t");
-scanf("%d",&n);
-for(i=n;i>1;i--)
-{
-	f=f*i;
+int main(){
+int a;
+    printf("Enter the number whose factorial is to be calculated: ");
+    scanf("%d", &a);    
+    printf("The value of factorial %d is %d", a, factorial(a));
+    return 0;
 }
-printf("The factorial of %d is %d",n,f);
-return 0;
+int factorial(int x){
+    printf("Calling factorial(%d)\n", x);
+    if (x==1 || x==0){
+        return 1;
+    }
+    else{
+        return x * factorial(x-1);
+    }
 }
+	
