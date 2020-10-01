98f64b1030f0a0351be5359d47b61420886e961e
Shyam Radadiya <67785558+shyam-ra18@users.noreply.github.com>
2020-10-01 10:51:19 +0530

    Update add 2no.c

--- add 2no.c
+++ add 2no.c
@@ -2,8 +2,15 @@
  main()
 {
 	int a , b, sum;
-	printf("Enter two numbers:");
-	scanf("%d%d",&a,&b);
+	printf("Enter first number:");
+	scanf("%d",&a);
+	 
+	 
+	printf("Enter second number:");
+	scanf("%d",&b);
+	 
 	sum=a+b;
-	printf("%d + %d = %d",a,b,sum);
+	printf("The sum of %d and %d is %d",a,b,sum);
+	 
+	 return 0; //Don't forgot this..
 }
