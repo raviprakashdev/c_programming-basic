fe9bafe6fa797a41f48b37cc740e88eb913b89c2
PraNjaL-16 <54860381+PraNjaL-16@users.noreply.github.com>
2020-10-12 12:13:21 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,18 +1,32 @@
-#include<stdio.h> 
-#include<math.h> 
-int fib(int n) 
-{ 
-   if (n <= 1) 
-      return n; 
-   return fib(n-1) + fib(n-2); 
-} 
-  
-int main () 
-{ 
-  int n;
-  printf("enter a number");
-  scanf("%d",&n);
-  printf("%d", fib(n)); 
-  getchar(); 
-  return 0; 
-} 
+#include <stdio.h>
+
+int factorial(int n) {
+   //base case
+   if(n == 0) {
+      return 1;
+   } else {
+      return n * factorial(n-1);
+   }
+}
+
+int fibbonacci(int n) {
+   if(n == 0){
+      return 0;
+   } else if(n == 1) {
+      return 1;
+   } else {
+      return (fibbonacci(n-1) + fibbonacci(n-2));
+   }
+}
+
+int main() {
+   int n = 5;
+   int i;
+	
+   printf("Factorial of %d: %d\n" , n , factorial(n));
+   printf("Fibbonacci of %d: " , n);
+	
+   for(i = 0;i<n;i++) {
+      printf("%d ",fibbonacci(i));            
+   }
+}
